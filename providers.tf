bd0bbffc689fc3bd438756602cc0379be9fab683
Rahul Rawla <rahul.rawla@candidpartners.com>
2020-10-16 19:16:54 -0400

    fix issue

--- providers.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  version = "~> 3.0"
-  region   = "ca-central-1"
-  assume_role {
-    role_arn     = "arn:aws:iam::005901988046:role/pipeline-services-terraform"
-    session_name = "pipeline-services"
-  }
-}
