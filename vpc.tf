6df04aac1340857dd694ca6d5c5ed1fbcb76580f
Rahul Rawla <rahul.rawla@candidpartners.com>
2020-10-18 19:20:16 -0400

    added vpc terraform

---
+++ vpc.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "compliance-test" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name    = "compliance vpc",
+    Purpose = "Testing compliance integration"
+  }
+}
+data "aws_s3_bucket" "flowlog" {
+  bucket = "rahul.jenkins.test"
+}
+resource "aws_flow_log" "example" {
+  log_destination      = data.aws_s3_bucket.flowlog.arn
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.compliance-test.id
+}
